0c139b7907e874b67da6acde57ee275766e96a88
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-01-24 16:40:42 +0200

    Go ahead 2

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,35 +1,35 @@
-import React, {Component} from "react";
-import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
-import { Formik, ErrorMessage } from "formik";
-import * as yup from 'yup';
-import {FormStyled, FieldStyled, LabelStyled} from './Form.styled'
+// import React, {Component} from "react";
+// import PropTypes from 'prop-types';
+// import { nanoid } from 'nanoid';
+// import { Formik, ErrorMessage } from "formik";
+// import * as yup from 'yup';
+// import {FormStyled, FieldStyled, LabelStyled} from './Form.styled'
 
-const schema = yup.object().shape({
-  name: yup.string().required(),
-  number: yup.number().min(1000000).max(9999999).integer().required(),
-})
+// const schema = yup.object().shape({
+//   name: yup.string().required(),
+//   number: yup.number().min(1000000).max(9999999).integer().required(),
+// })
 
-class ContactForm extends Component {
-    state = {
-        name: '',
-        number: ''
-    }
+// class ContactForm extends Component {
+//     state = {
+//         name: '',
+//         number: ''
+//     }
 
     // handleChange = event => {
     //     const {name, value} = event.currentTarget;
     //     this.setState( {[name]: value} );
     //   }
 
-    handleSubmit = (values, {resetForm}) => {
+    // handleSubmit = (values, {resetForm}) => {
 
-        const { name, number } = values;
-        const contact = ({
-          id: nanoid(5), name: name, number: number
-        })
-        this.props.addContact(contact); 
-        // this.reset();
-        resetForm();
+    //     const { name, number } = values;
+    //     const contact = ({
+    //       id: nanoid(5), name: name, number: number
+    //     })
+    //     this.props.addContact(contact); 
+    //     // this.reset();
+    //     resetForm();
       }
 
       // reset = () => {
@@ -39,38 +39,38 @@ class ContactForm extends Component {
       //   });
       // };
 
-    render(){
-        // const { name, number } = this.state;
-    return (
-          <div> 
-            <Formik initialValues={this.state} onSubmit={this.handleSubmit} validationSchema={schema}>
-              <FormStyled autoComplete='off'>
-                <LabelStyled>
-                <label htmlFor="name">
-                  Name <br/>
-                  <FieldStyled type="text" name="name" required /><br/>
-                  <ErrorMessage name="name" />
-                </label>
-                </LabelStyled>
-                <LabelStyled>
-                <label htmlFor="number">
-                  Phone <br/>
-                  <FieldStyled type="tel" name="number" required /><br/>
-                  <ErrorMessage name="number" />
-                </label>
-                </LabelStyled>
-                <button type='submit'>Add contact</button>
-              </FormStyled>
-            </Formik>
-          </div>
-   )}}
+//     render(){
+//         // const { name, number } = this.state;
+//     return (
+//           <div> 
+//             <Formik initialValues={this.state} onSubmit={this.handleSubmit} validationSchema={schema}>
+//               <FormStyled autoComplete='off'>
+//                 <LabelStyled>
+//                 <label htmlFor="name">
+//                   Name <br/>
+//                   <FieldStyled type="text" name="name" required /><br/>
+//                   <ErrorMessage name="name" />
+//                 </label>
+//                 </LabelStyled>
+//                 <LabelStyled>
+//                 <label htmlFor="number">
+//                   Phone <br/>
+//                   <FieldStyled type="tel" name="number" required /><br/>
+//                   <ErrorMessage name="number" />
+//                 </label>
+//                 </LabelStyled>
+//                 <button type='submit'>Add contact</button>
+//               </FormStyled>
+//             </Formik>
+//           </div>
+//    )}}
    
-export default ContactForm;
+// export default ContactForm;
 
-ContactForm.propTypes = {
-     addContact: PropTypes.func.isRequired,
-     };
+// ContactForm.propTypes = {
+//      addContact: PropTypes.func.isRequired,
+//      };
 
 
      
-const key = 36683079-f7ca8efdb46bf14669a93b6f2;
+// const key = 36683079-f7ca8efdb46bf14669a93b6f2;
